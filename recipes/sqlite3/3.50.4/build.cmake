52925b744aeb9af627dc0054679675236ad181b2
Chris Austen <christopher.austen@amd.com>
2025-09-25 09:49:05 -0400

    skip -lm for Windows

--- recipes/sqlite3/3.50.4/build.cmake
+++ recipes/sqlite3/3.50.4/build.cmake
@@ -24,7 +24,11 @@ target_link_libraries(sqlite3 ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_DL_LIBS})
 
 add_executable(shell shell.c)
 target_link_libraries(shell sqlite3)
-target_link_options(shell PRIVATE "LINKER:-lm")
+
+if(NOT WIN32)
+    target_link_options(shell PRIVATE "LINKER:-lm")
+endif()
+
 set_target_properties(shell PROPERTIES OUTPUT_NAME sqlite3)
 
 set(prefix ${CMAKE_INSTALL_PREFIX})
